379099d87fa9c3ba66b0bf8604943b18eec21646
LinSijie <linsijie17@163.com>
2020-04-01 20:36:32 -0400

    Support detecting multiple beacons.

--- BeaconDetector/ContentView.swift
+++ BeaconDetector/ContentView.swift
@@ -11,8 +11,7 @@ import SwiftUI
  
  class BeaconScanner: NSObject, ObservableObject, CLLocationManagerDelegate {
     var locationManager: CLLocationManager?
-    @Published var lastDistance = CLProximity.unknown
-    @Published var lastRSSI = -99
+    @Published var lastBeacons: [CLBeacon] = []
 
     override init() {
         super.init()
@@ -39,7 +38,7 @@ import SwiftUI
     func startScanning() {
         print("Start Scanning")
         let uuid = UUID(uuidString: "5a4bcfce-174e-4bac-a814-092e77f6b7e5")!
-        let constraint = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid)
         let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: "MyBeacon")
         locationManager?.startMonitoring(for: beaconRegion)
         locationManager?.startRangingBeacons(satisfying: constraint)
@@ -47,83 +46,28 @@ import SwiftUI
 
     func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
         print("didRange")
-        if let beacon = beacons.first {
-            print("beacon = beacons.first")
-            update(distance: beacon.proximity, rssi: beacon.rssi)
+        if beacons.isEmpty {
+            print("beacon not found")
+            update(beacons: [])
         } else {
-            print("beacon = unknown")
-            update(distance: .unknown, rssi: -99)
+            print(String(beacons.count) + " beacons found")
+            update(beacons: beacons)
         }
     }
 
-    func update(distance: CLProximity, rssi: Int) {
-        print("update lastdistance")
-        lastDistance = distance
-        lastRSSI = rssi
+    func update(beacons: [CLBeacon]) {
+        print("update last beacon array")
+        lastBeacons = beacons
     }
  }
  
- struct BigText: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(Font.system(size: 72, design: .rounded))
-    }
- }
 
-struct ContentView: View {
+ struct ContentView: View {
     @ObservedObject var detector = BeaconScanner()
-    
     var body: some View {
-        if detector.lastDistance == .immediate {
-            return VStack {
-                VStack {
-                    Text("IMMEDIATE")
-                        .font(Font.system(size: 72, design: .rounded))
-                    Text("RSSI = " + String(detector.lastRSSI))
-                        .font(Font.system(size: 36, design: .rounded))
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .background(Color.red)
-                .edgesIgnoringSafeArea(.all)
-            }
-        } else if detector.lastDistance == .near {
-            return VStack {
-                VStack {
-                    Text("NEAR")
-                        .font(Font.system(size: 72, design: .rounded))
-                    Text("RSSI = " + String(detector.lastRSSI))
-                        .font(Font.system(size: 36, design: .rounded))
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .background(Color.orange)
-                .edgesIgnoringSafeArea(.all)
-            }
-        } else if detector.lastDistance == .far {
-            return VStack {
-                VStack {
-                    Text("FAR")
-                        .font(Font.system(size: 72, design: .rounded))
-                    Text("RSSI = " + String(detector.lastRSSI))
-                        .font(Font.system(size: 36, design: .rounded))
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .background(Color.blue)
-                .edgesIgnoringSafeArea(.all)
-            }
-        } else {
-            return VStack {
-                VStack {
-                    Text("UNKNOWN")
-                        .font(Font.system(size: 72, design: .rounded))
-                    Text("RSSI = " + String(detector.lastRSSI))
-                        .font(Font.system(size: 36, design: .rounded))
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .background(Color.gray)
-                .edgesIgnoringSafeArea(.all)
-            }
+        List(detector.lastBeacons, id: \.major) { beacon in
+          BeaconInfoView(beacon: beacon)
         }
-//        Text("RSSI: ")
     }
 }
 
