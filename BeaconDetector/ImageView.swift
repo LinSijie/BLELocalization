8a6cb0d5ac4dc5ee36a0e25fe8a9a79f493c646d
XiaZhengyang <zhengyax@andrew.cmu.edu>
2020-04-08 17:52:58 -0400

    support marker display

---
+++ BeaconDetector/ImageView.swift
@@ -0,0 +1,21 @@
+//
+//  ImageView.swift
+//  BeaconDetector
+//
+//  Created by Xia Zhengyang on 2020/4/8.
+//  Copyright © 2020 林思劼. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
