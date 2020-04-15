796462aa5c85aa5bffd421b8938cf538e9404b83
XiaZhengyang <zhengyax@andrew.cmu.edu>
2020-04-15 16:02:51 -0400

    fix frame size and coordinate translation

--- BeaconDetector/ContentView.swift
+++ BeaconDetector/ContentView.swift
@@ -64,6 +64,7 @@ import SwiftUI
         print("update last beacon array")
         lastBeacons = []
         for beacon in beacons {
+            print(String(beacon.rssi))
             lastBeacons.append(identifiableBeacon(beacon: beacon))
         }
     }
@@ -86,12 +87,15 @@ import SwiftUI
     var body: some View {
         VStack{
             ZStack{
-                Image("map").resizable()
+                Image("map")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400, height: 622)
                 ForEach(detector.lastBeacons) { identifiableBeacon in
                     Triangle()
                     .fill(Color.blue)
                     .frame(width: 10, height: 10)
-                        .offset(x: CGFloat(truncating: identifiableBeacon.beacon.major), y: CGFloat(truncating: identifiableBeacon.beacon.minor))
+                        .offset(x: CGFloat(truncating: identifiableBeacon.beacon.major)-200, y: CGFloat(truncating: identifiableBeacon.beacon.minor)-311)
                 }
             }
     
